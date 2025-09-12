1653a401d03c007898ad1b95c17610d9469b3609
alfandisafira <alfandisafira@gmail.com>
2025-09-12 23:08:52 +0700

    add: file Enum Status

---
+++ library-management-system/src/main/java/com/sinaukoding/tugas_akhir/library_management_system/model/enums/Status.java
@@ -0,0 +1,17 @@
+package com.sinaukoding.tugas_akhir.library_management_system.model.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Status {
+
+    AKTIF("Aktif"),
+    TIDAK_AKTIF("Tidak Aktif");
+
+    private final String label;
+
+    Status(String label) {
+        this.label = label;
+    }
+
+}
